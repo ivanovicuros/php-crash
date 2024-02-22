33394215890c4b504265d1682268f03fe27700a9
Uros <urosivanovich@gmail.com>
2024-02-21 22:44:42 -0500

    sanitze input

--- _starter_files/11_sanitizing_inputs.php
+++ _starter_files/11_sanitizing_inputs.php
@@ -4,3 +4,40 @@
 /*
   Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
 */
+if (isset($_POST['submit'])) {
+
+  // htmlspecialchars() - Convert special characters to HTML entities
+  // $name = htmlspecialchars($_POST['name']);
+  // $email = htmlspecialchars($_POST['email']);
+
+  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+
+  // filter_var() - Sanitize data
+  // $name = filter_var($_POST['name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  // $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+  echo $name;
+
+  // FILTER_SANITIZE_STRING - Convert string to string with only alphanumeric, whitespace, and the following characters - _.:/
+  // FILTER_SANITIZE_EMAIL - Convert string to a valid email address
+  // FILTER_SANITIZE_URL - Convert string to a valid URL
+  // FILTER_SANITIZE_NUMBER_INT - Convert string to an integer
+  // FILTER_SANITIZE_NUMBER_FLOAT - Convert string to a float
+  // FILTER_SANITIZE_FULL_SPECIAL_CHARS - HTML-encodes special characters, keeps spaces and most other characters
+
+}
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"
+method="POST">
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" name="name">
+  </div>
+  <div>
+    <label for="age">Age:</label>
+    <input type="text" name="age">
+  </div>
+  <input type="submit" value="Submit" name="submit">
+</form>
