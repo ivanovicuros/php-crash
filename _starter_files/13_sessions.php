94f26ce74fc8f2a0eb64f465733c9b69123b5786
Uros <urosivanovich@gmail.com>
2024-02-24 14:45:10 -0500

    sessions

--- _starter_files/13_sessions.php
+++ _starter_files/13_sessions.php
@@ -5,3 +5,37 @@
   Sessions are a way to store information (in variables) to be used across multiple pages.
   Unlike cookies, sessions are stored on the server.
 */
+
+session_start(); // Must be called before accessing any session data
+
+if(isset($_POST['submit'])) {
+  $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+
+  $password = $_POST['password'];
+
+  if($username == 'john' && $password == 'password') {
+    $_SESSION['username'] = $username;
+    header('Location: /php-crash/extras/dashboard.php');
+  } else {
+    echo 'Incorrect Login';
+  }
+}
+
+?>
+
+
+<form action="<?php echo htmlspecialchars(
+  $_SERVER['PHP_SELF']
+); ?>" method="POST">
+  <div>
+    <label>Username: </label>
+    <input type="text" name="username">
+  </div>
+  <br>
+  <div>
+    <label>Password: </label>
+    <input type="password" name="password">
+  </div>
+  <br>
+  <input type="submit" name="submit" value="Submit">
+</form>
